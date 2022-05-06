ec7f303bf8288a31d59579a259a7261a26fb788d
Augus <bramlye@outlook.com>
2022-05-06 14:47:38 +0800

    feat: small scale chopper

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # rust
+
+> following [Rust Course](https://course.rs/)
+
+## Preliminary exploration
+
+1. build and run: `cargo run`
+2. build: `cargo build`
+3. run: `./target/debug/rust`
+4. release build and run: `cargo run --release`
+5. release build: `cargo build --release`
+6. `cargo check`: 当项目大了后，`cargo run` 和 `cargo build` 不可避免的会变慢。此时，使用 `cargo check`
+   快速的检查一下代码能否编译通过。因此该命令速度会非常快，能节省大量的编译时间。
+
+
+- `Cargo.toml` 是 cargo 特有的项目数据描述文件。它存储了项目的所有元配置信息，如果 Rust 开发者希望 Rust 项目能够按照期望的方式进行构建、测试和运行，那么，必须按照合理的方式构建 Cargo.toml。
+- `Cargo.lock` 文件是 cargo 工具根据同一项目的 toml 文件生成的项目依赖详细清单，因此我们一般不用修改它，只需要对着 Cargo.toml 文件撸就行了。
+  > 什么情况下该把 Cargo.lock 上传到 git 仓库里？很简单，当你的项目是一个可运行的程序时，就上传 Cargo.lock，如果是一个依赖库项目，那么请把它添加到 .gitignore 中
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+fn greet_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好！";
+    let english = "World, hello!";
+    let italian = "mondo, Ciao!";
+    let french = "Le monde, Bonjour!";
+    let russian = "мир, Привет!";
+    let korean = "세계, 안녕하세요!";
+    let german = "Welt, Hallo!";
+    let regions = [southern_germany, chinese, english, italian, french, russian, korean, german];
+    for region in regions {
+        println!("{}", &region);
+    }
+}
+
+fn small_scale_chopper() {
+    let penguin_data = "\
+common name,length (cm)
+Little penguin,33
+Yellow-eyed penguin,65
+Fiordland penguin,60
+Invalid,data
+";
+    let records = penguin_data.lines();
+    for (i, record) in records.enumerate() {
+        if i == 0 || record.trim().len() == 0 { continue; }
+        let fields: Vec<_> = record.split(',').map(|field| field.trim()).collect();
+        if cfg!(debug_assertions) { eprintln!("debug: {:?} -> {:?}", record, fields); }
+
+        let name = fields[0];
+        if let Ok(length) = fields[1].parse::<f32>() { println!("{}, {}cm", name, length) }
+    }
+}
+
+fn variables() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    let y = 3;
+    let _z = 44;
+}
+
 fn main() {
-    println!("Hello, world!");
+    greet_world();
+    small_scale_chopper();
+    variables();
 }
